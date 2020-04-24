cb2d374f0a1c48f90e25dcbfa45ec7a22d45c7a3
DishaVaghamshi <dvvaghamshi18@gmail.com>
2020-04-23 20:10:45 -0400

    commit for todocontroller

--- todo/todo/src/main/java/com/todo/web/controller/TodoController.java
+++ todo/todo/src/main/java/com/todo/web/controller/TodoController.java
@@ -36,14 +36,17 @@ public class TodoController {
 		}
 	@RequestMapping(value = "/list-todo", method = RequestMethod.GET)
 	public String showTodos(ModelMap model) {
-		String name = (String) model.get("name");
+		String name = getLoggedInUserName(model);
 		model.put("todos", service.retrieveTodos(name));
 		return "list-todo";
+	}
+	private String getLoggedInUserName(ModelMap model) {
+		return (String) model.get("name");
 	}
 
 	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 	public String showAddTodoPage(ModelMap model) {
-		model.addAttribute("todo", new Todo(0, (String) model.get("name"),
+		model.addAttribute("todo", new Todo(0, getLoggedInUserName(model),
 				"Default Desc", new Date(), false));
 		return "todo";
 	}
@@ -68,7 +71,7 @@ public class TodoController {
 			return "todo";
 		}
 		
-		todo.setUser((String) model.get("name"));
+		todo.setUser(getLoggedInUserName(model));
 		
 		service.updateTodo(todo);
 
@@ -82,7 +85,7 @@ public class TodoController {
 			return "todo";
 		}
 
-		service.addTodo((String) model.get("name"), todo.getDesc(), todo.getTargetDate(),
+		service.addTodo(getLoggedInUserName(model), todo.getDesc(), todo.getTargetDate(),
 				false);
 		return "redirect:/list-todo";
 	}
