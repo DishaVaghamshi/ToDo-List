94dc8820e97f71c23469bd10650e35518da45755
DishaVaghamshi <dvvaghamshi18@gmail.com>
2020-04-23 22:32:51 -0400

    commit for welcome controller

--- todo/todo/src/main/java/com/todo/web/controller/WelcomeController.java
+++ todo/todo/src/main/java/com/todo/web/controller/WelcomeController.java
@@ -1,6 +1,8 @@
 package com.todo.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.todo.web.service.LoginService;
+//import com.todo.web.service.LoginService;
 
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 
-	@Autowired
-	LoginService service;
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String showLoginPage(ModelMap model){
-		model.put("name","disha");
+	public String showWelcomePage(ModelMap model){
+		model.put("name",getLoggedInUserName());
 		return "welcome";
 	}
 	
+	private String getLoggedInUserName() {
+		Object principal = SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails)
+			return ((UserDetails) principal).getUsername();
+
+		return principal.toString();
+	}
 }
